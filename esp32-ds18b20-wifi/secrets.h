83a18b951a5980f0010639291c712a647283ce42
Helder Correia <tuxboy@tlab.pt>
2024-01-23 20:30:43 +0000

    esp32-ds18b20-wifi

---
+++ esp32-ds18b20-wifi/secrets.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID "CHANGE-ME"
+#define SECRET_PASS "CHANGE-ME"
+
+#define MQTT_HOST "CHANGE-ME"
+#define MQTT_USER "CHANGE-ME"
+#define MQTT_PORT 1883
+#define MQTT_PASS "CHANGE-ME"
+#define MQTT_TOPIC "pixel/ds18b20/temperature"
